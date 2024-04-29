f508811dd12501e54d62c37fbbf106ca15a16587
farouq <48164417+nomaddevs1@users.noreply.github.com>
2024-04-29 11:49:31 -0500

    Update config.py

--- server/config.py
+++ server/config.py
@@ -6,7 +6,7 @@ OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
 ADDRESS = os.getenv("APP_ADDRESS", "http://127.0.0.1")
 CLIENT_URL = os.getenv("APP_CLIENT_URL", "http://localhost:3000")
 PORT = os.getenv("APP_PORT", "8000")
-HOST_URL = f"{ADDRESS}:{PORT}"
+HOST_URL = f"https://{os.getenv('VERCEL_URL')}"
 LOG_FILE = os.getenv("APP_LOG_FILE")
 MODE = os.getenv("APP_MODE", "DEV")
 
